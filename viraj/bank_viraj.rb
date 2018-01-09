06fa31f341af6599788ef69ff62ca55939ed8a36
Viraj Chavan <virajc@live.com>
2018-01-09 15:07:28 +0530

    bank program: more modular

--- viraj/bank_viraj.rb
+++ viraj/bank_viraj.rb
@@ -1,4 +1,4 @@
-#bank program
+#bank program (Before: 167 lines)
 
 #BUG: enetering non-numeric data where numeric input is expected doesn't raise an error
 #Make things more MODULAR
@@ -13,7 +13,7 @@ users_list = {}					#no user exists in the beginning
 $logged_in_user_id = false		#false if user not logged in
 
 
-#saves to the hash users_list and returns the newly created user_id
+#saves to the hash 'users_list' and returns the newly created user_id
 def register(users_list, password)
 	#new user_id is generated using the lenght of the hash 'users_list'
 	#a different approach should be used to generate user_id if the function to delete a user is implemented
@@ -25,38 +25,41 @@ def register(users_list, password)
 	$logged_in_user_id = new_user_id
 	puts "Your ID is #{$logged_in_user_id}. \nUser #{$logged_in_user_id} is registered and logged in."
 
-	return new_user_id
+	new_user_id
+end
+
+def register_caller(users_list, user_id)
+	puts "\nUser #{user_id} is not registered.\nWould you like to register?(y/n)"
+	if gets.chomp.eql? 'y'
+		puts 'Enter your new password: '
+		$logged_in_user_id = register(users_list, gets.chomp)
+		true
+	else
+		false 	#user doesn't want to register
+	end
+end
+
+def login
+	puts 'Enter your password: '
+	if gets.to_s.chomp == users_list[user_id][:password]
+		$logged_in_user_id = user_id
+		puts "User #{user_id} logged in."
+		true
+	else
+		puts 'Wrong password'
+		false
+	end	
 end
 
 
-#if user exists => asks user for password and log in,
-#if doesn't => gives an option to register
-def login(users_list, user_id)
+def login_caller(users_list, user_id)
 	#if user does exist
 	if users_list[user_id]
-		puts 'Enter your password: '
-		entered_password = gets
-		if entered_password.to_s.chomp == users_list[user_id][:password]
-			$logged_in_user_id = user_id
-			puts "User #{user_id} logged in."
-			return true
-		else
-			puts 'Wrong password'
-			return false
-		end
+		login(users_list, user_id)
 
 	#if user doesn't exist
 	else
-		puts "\nUser #{user_id} is not registered.\nWould you like to register?(y/n)"
-		register_or_not = gets
-		if register_or_not.chomp.eql? 'y'
-			puts 'Enter your new password: '
-			password = gets
-			$logged_in_user_id = register(users_list, password.chomp)
-			return true
-		else
-			exit 		#exit because user doesn't want to register
-		end
+		register_caller(users_list, user_id)
 	end
 end
 
@@ -89,13 +92,43 @@ end
 
 
 def logout
+	#assign value to the global variable
 	$logged_in_user_id = false
 end
 
+def deposit_caller(users_list)
+	system "clear"
+	puts 'Enter amount to be deposited: '
+	amount = gets				
+	deposit(users_list, amount.to_i)
+	puts 'Deposit successful.'	
+end
+
+def withdraw_caller(users_list)
+	system "clear"
+	puts 'Enter amount to be withdrawn: '
+	amount = gets
+	withdraw(users_list, amount.to_i)	
+end
+
+def transfer_caller(users_list)
+	system "clear"
+	puts 'Enter user ID to transfer money: '
+	transfer_user_id = gets.to_i
+	if(transfer_user_id != $logged_in_user_id)					
+		if users_list[transfer_user_id]
+			puts 'Enter amount to transfer: '
+			amount = gets.to_i
+			transfer(users_list, transfer_user_id, amount)
+		else
+			puts 'The user ID you entered does not exist.'
+		end
+	else
+		puts 'You cannot transfer to your own account.'
+	end	
+end
 
 #execution starts here:
-
-#loops until chooses not to log in/register
 while true
 	puts "To login, enter your ID: (Enter 0 to exit)"
 	user_id = gets.chomp.to_i
@@ -105,59 +138,29 @@ while true
 		break
 	end
 
-	if login(users_list, user_id)
-		#user is logged in, provide user with actions to choose
-
-		#the global variable $logged_in_user_id is used to refer to the logged in user at all the time
-
+	if login_caller(users_list, user_id)
 		#loops until user logs out
 		while true
-			puts "\n[Your Balance: #{users_list[$logged_in_user_id][:balance]}]
-			Choose an action: 
-			1. Deposit
-			2. Withdraw
-			3. Transfer
-			4. Logout"
+			puts "______________________"
+			puts "\n[Your Balance: #{users_list[$logged_in_user_id][:balance]}]"
+			puts "______________________"
+			puts "\n1. Deposit\n2. Withdraw\n3. Transfer\n4. Logout"
 
-			action = gets.to_i
-
-			if action == 1
-				#deposit
-				puts 'Enter amount to be deposited: '
-				amount = gets
-				deposit(users_list, amount.to_i)
-				puts 'Deposit successful.'
-			end
-
-			if action == 2
-				#withdraw
-				puts 'Enter amount to be withdrawn: '
-				amount = gets
-				withdraw(users_list, amount.to_i)
-			end
-
-			if action == 3
-				#transfer
-				puts 'Enter user ID to transfer money: '
-				transfer_user_id = gets.to_i
-				if(transfer_user_id != $logged_in_user_id)					
-					if users_list[transfer_user_id]
-						puts 'Enter amount to transfer: '
-						amount = gets.to_i
-						transfer(users_list, transfer_user_id, amount)
-					else
-						puts 'The user ID you entered does not exist.'
-					end
+			case gets.to_i
+				when 1
+					deposit_caller(users_list)
+				when 2
+					withdraw_caller(users_list)
+				when 3
+					transfer_caller(users_list)
+				when 4
+					logout
+					break
 				else
-					puts 'You cannot transfer to your own account.'
-				end
-			end
-
-			if action == 4
-				logout
-				break
+					puts "Enter valid option."
 			end
 		end
 	end
 end
+
 
--- viraj/symbols.rb
+++ viraj/symbols.rb
@@ -4,4 +4,13 @@ def add(number1, number2)
 	number1 + number2
 end
 
+=begin
+print add(2,3)
+print add(1,2)
+=end
+
 puts add(2,3)
+puts add(1,2)
+
+
+
