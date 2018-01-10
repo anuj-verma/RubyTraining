e7adb086d50cbe9641f7d6e33d7094e73e689aae
Nikhil Pathak <nikhilpathak926@gmail.com>
2018-01-10 15:26:40 +0530

    Used Block for User Input

--- nikhil/banking.rb
+++ nikhil/banking.rb
@@ -5,14 +5,9 @@ user_info = {user101: {name: "Nikhil Pathak", password: "user101", balance: 0.0,
 			user102: {name: "Saransh Khandelwal" , password: "user102", balance: 0.0, 
 			city: "Agra", transaction_history: []}}
 
-def string_input(display_string)
-	puts display_string
-	gets.chop
-end
-
-def float_input(display_string)
-	puts display_string
-	gets.chop.to_f
+def input(message)
+	puts message
+	yield(gets.chop) if block_given?
 end
 
 def user_exist(user_info, account_number)
@@ -73,6 +68,7 @@ def transfer(user_info, account_number, reciever_account_number, amount, date)
 			user_info[account_number.to_sym][:balance] -= amount
 			user_info[reciever_account_number.to_sym][:balance] += amount
 			add_transaction_history(user_info, account_number, 'Transfer', amount, date)
+			add_transaction_history(user_info, account_number, 'Deposit', amount, date)
 			puts 'Successfully Transfered Amount'
 		else
 			puts 'Insufficient Balance to Transfer!'
@@ -106,20 +102,16 @@ def user_registeration(user_info)
 	puts 'Please Register before banking'
 	new_user = {}
 
-	puts 'Enter your Name'
-	new_user[:name] = gets.chop
+	new_user[:name] = input('Enter your Name') { |input_value| input_value }
 	new_user[:name] = validate_string_input(new_user[:name], "Name")
 
-	puts 'Enter Password'
-	new_user[:password] = gets.chop
+	new_user[:password] = input('Enter Password') { |input_value| input_value }
 	new_user[:password] = validate_string_input(new_user[:balance], "Password")
 
-	puts 'Enter Initial Balance'
-	new_user[:balance] = gets.chop.to_f
+	new_user[:balance] = input('Enter Initial Balance') { |input_value| input_value.to_f }
 	new_user[:balance] = validate_string_input(new_user[:balance], "Initial Balance")
 
-	puts 'Enter City'
-	new_user[:city] = gets.chop
+	new_user[:city] = input('Enter City') { |input_value| input_value }
 	new_user[:city] = validate_string_input(new_user[:city], "City")
 
 	new_user[:transaction_history] = []
@@ -153,7 +145,7 @@ incorrect_password_flg = 0
 while 1
 
 	if incorrect_password_flg == 0
-		account_number = string_input('Enter your Account Number')
+		account_number = input('Enter your Account Number') { |input_value| input_value}
 		account_number = validate_string_input(account_number, "Account Number")
 	
 		account_number = user_exist(user_info, account_number)
@@ -163,41 +155,37 @@ while 1
 		end
 	end
 
-	password = string_input('Enter Your Password')
+	password = input('Enter Your Password') { |input_value| input_value}
 	password = validate_string_input(password, "Password")
 	if user_authentication(user_info, account_number, password) == 1	
 		while 1	
 			option = operations_details().to_i
 			if option != 4 and option != 5 and option != 6
 				current_time = DateTime.now 
-				current_time = current_time.strftime("%d/%m/%Y %H:%M")
-				current_time = Date.parse(current_time)
 			end
 			case option
 				when 1
-					amount = float_input('Enter Withdrawal Amount')
+					amount = input('Enter Withdrawal Amount') { |input_value| input_value.to_f }
 					amount = validate_input_amount(amount) 
 					withdraw(user_info, account_number, amount, current_time)
 
 				when 2
-					amount = float_input('Enter Deposit Amount')
+					amount = input('Enter Deposit Amount') { |input_value| input_value.to_f }
 					amount = validate_input_amount(amount)
 					deposit(user_info, account_number, amount, current_time)
 
 				when 3
-					reciever_account_number = float_input('Enter another user account number to transfer money')
+					reciever_account_number = input('Enter another user account number to transfer money') { |input_value| input_value }
 					reciever_account_number = validate_string_input(reciever_account_number, "Amount to Transfer")
-					amount = float_input('Enter Amount to Transfer')
+					amount = input('Enter Amount to Transfer') { |input_value| input_value.to_f }
 					amount = validate_input_amount(amount)
 					transfer(user_info, account_number, reciever_account_number, amount, current_time)
 			
 				when 4
-					start_date = string_input('Enter Start Date')
-					start_date += '00:00'
-					start_date = Date.parse(start_date)
-					end_date = string_input('Enter End Date')
-					end_start += '00:00'
-					end_date = Date.parse(end_date)
+					start_date = input('Enter Start Date') { |input_value| input_value }
+					start_date = DateTime.parse(start_date)
+					end_date = input('Enter End Date') { |input_value| input_value }
+					end_date = DateTime.parse(end_date)
 					display_transaction_history(user_info, account_number, start_date, end_date)
 
 				when 5
