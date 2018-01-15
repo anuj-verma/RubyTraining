5f75a6cd30bf38d64692f016e65065deab863ddb
Viraj Chavan <virajc@live.com>
2018-01-15 18:05:44 +0530

    wip

--- viraj/restos.rb
+++ viraj/restos.rb
@@ -1,53 +1,65 @@
 class Resto
-	attr_reader :name, :menu
-	def initialize(name, menu)
+	attr_reader :name, :dishes
+	def initialize(name, dishes)
 		@name = name
-		#(e.g) menu = [{name: 'Rajma', price: 30}, {name: 'Dal', price: 20}]
-		@menu = menu
+		@dishes = dishes
 	end
 
-	def search_menu(query)
-		name if menu.include?(query)
+	def search_dishes(query)
+		dishes.each { | dish |
+			return name if(dish.name.downcase == query.name.downcase && dish.price == query.price)
+		}
+		false
 	end
 end
 
-class RestoFinder
+class Dish
+	attr_reader :name, :price
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
+end
+
+class Main
+	attr_reader :restos, :query
 	def initialize
-		@restos = input_info
-		@query = input_query
+		@restos = get_info
+		@query = get_query
 	end
 
-	def input_query
+	def get_query
 		puts 'Enter dish name:'
 		dish_name = gets.chomp.to_s
 		puts 'Enter price: '
 		dish_price = gets.chomp.to_i
-		{ name: dish_name, price: dish_price }
+		Dish.new(dish_name, dish_price)
 	end
 
 	def find_restos
 		result = []
-		for resto in @restos
-			result << resto.search_menu(@query) if resto.search_menu(@query)
+		for resto in restos
+			result << resto.search_dishes(query) if resto.search_dishes(query)
 		end
 		result
 	end
 
-	def input_info
+	def get_info
 		restos = []
-		restos << Resto.new('Box8', [{name: 'Rajma', price: 30},
-																			{name: 'Dal', price: 20},
-																			{name: 'Vada Pav', price: 50}])
-		restos << Resto.new('Loacvore',[{name: 'Sabji', price: 40},
-																					{name: 'Dal', price: 10},
-																		 			{name: 'Vada Pav', price: 30}])
+		restos << Resto.new('Box8', [Dish.new('Rajma', 30),
+																			Dish.new('Dal', 20),
+																			Dish.new('Vada Pav', 50)])
+		restos << Resto.new('Loacvore', [Dish.new('Sabji', 40),
+																			Dish.new('Dal', 10),
+																			Dish.new('Vada Pav', 30)])
 
-		restos << Resto.new('Flavours',[{name: 'Ice cream', price: 10},
-																		 			{name: 'Sandwich', price: 20},
-																		 			{name: 'Vada Pav', price: 30},
-																		 			{name: 'Burger', price: 30}])
+		restos << Resto.new('Box8', [Dish.new('Ice cream', 10),
+																			Dish.new('Sandwich', 20),
+																			Dish.new('Vada Pav', 30),
+																			Dish.new('Burger', 30)])
+
 		restos
 	end
 end
 
-p RestoFinder.new.find_restos
+p Main.new.find_restos
