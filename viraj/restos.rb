fc56b01d34152f1fb511c112c2dcedbcf2601a1a
Viraj Chavan <virajc@live.com>
2018-01-15 18:52:57 +0530

    better formatting

--- viraj/restos.rb
+++ viraj/restos.rb
@@ -1,65 +1,66 @@
 class Resto
-	attr_reader :name, :dishes
-	def initialize(name, dishes)
-		@name = name
-		@dishes = dishes
-	end
+  attr_reader :name, :dishes
+  def initialize(name, dishes)
+    @name = name
+    @dishes = dishes
+  end
 
-	def search_dishes(query)
-		dishes.each { | dish |
-			return name if(dish.name.downcase == query.name.downcase && dish.price == query.price)
-		}
-		false
-	end
+  def search_dishes(query)
+    dishes.each do |dish|
+      if dish.name.casecmp(query.name).zero? && dish.price == query.price
+        return name
+      end
+    end
+    false
+  end
 end
 
+
 class Dish
-	attr_reader :name, :price
-	def initialize(name, price)
-		@name = name
-		@price = price
-	end
+  attr_reader :name, :price
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
 
 class Main
-	attr_reader :restos, :query
-	def initialize
-		@restos = get_info
-		@query = get_query
-	end
+  attr_reader :restos, :query
+  def initialize
+    @restos = input_info
+    @query = input_query
+  end
 
-	def get_query
-		puts 'Enter dish name:'
-		dish_name = gets.chomp.to_s
-		puts 'Enter price: '
-		dish_price = gets.chomp.to_i
-		Dish.new(dish_name, dish_price)
-	end
+  def input_query
+    puts 'Enter dish name:'
+    dish_name = gets.chomp.to_s
+    puts 'Enter price: '
+    dish_price = gets.chomp.to_i
+    Dish.new(dish_name, dish_price)
+  end
 
-	def find_restos
-		result = []
-		for resto in restos
-			result << resto.search_dishes(query) if resto.search_dishes(query)
-		end
-		result
-	end
+  def find_restos
+    result = []
+    restos.each do |resto|
+      result << resto.search_dishes(query) if resto.search_dishes(query)
+    end
+    result
+  end
 
-	def get_info
-		restos = []
-		restos << Resto.new('Box8', [Dish.new('Rajma', 30),
-																			Dish.new('Dal', 20),
-																			Dish.new('Vada Pav', 50)])
-		restos << Resto.new('Loacvore', [Dish.new('Sabji', 40),
-																			Dish.new('Dal', 10),
-																			Dish.new('Vada Pav', 30)])
+  def input_info
+    restos = []
+    restos << Resto.new('Box8', [Dish.new('Rajma', 30),
+                                 Dish.new('Dal', 20),
+                                 Dish.new('Vada Pav', 50)])
+    restos << Resto.new('Loacvore', [Dish.new('Sabji', 40),
+                                     Dish.new('Dal', 10),
+                                     Dish.new('Vada Pav', 30)])
 
-		restos << Resto.new('Box8', [Dish.new('Ice cream', 10),
-																			Dish.new('Sandwich', 20),
-																			Dish.new('Vada Pav', 30),
-																			Dish.new('Burger', 30)])
+    restos << Resto.new('Box8', [Dish.new('Ice cream', 10),
+                                 Dish.new('Vada Pav', 30)])
 
-		restos
-	end
+    restos
+  end
 end
 
 p Main.new.find_restos
