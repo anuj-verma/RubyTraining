b56632b544819975462c486cb9d9a5188f29fb9b
JyotiSharma1234 <jyoti@joshsoftware.com>
2018-01-22 15:29:40 +0530

    facebook app just created

---
+++ facebook/db/migrate/20180122055730_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :gender
+      t.datetime :date_of_birth
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
